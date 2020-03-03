eab58612049d1ec433925b459bdf0a160ba4dadc
kubersharma001 <kubersharma001@gmail.com>
2020-03-03 14:14:23 +0530

    prebuilts: Import Simple-Calendar v6.8.4
    Source: https://github.com/SimpleMobileTools/Simple-Calendar/releases
    Credits: https://github.com/tibbi

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -1,2 +1,3 @@
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/DuckDuckGo)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/SimpleGallery)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/SimpleCalender)
