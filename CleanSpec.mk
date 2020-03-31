99543606eee4719031f8051fa669c340f376f593
ganeshi4u <ganeshvarmai4u@gmail.com>
2020-03-31 12:43:35 +0200

    prebuilts: Added WeatherIcons and stats
    Change-Id: I2759d1ea613fbd852cb31d73dc18cd27add87d4d

--- CleanSpec.mk
+++ CleanSpec.mk
@@ -1,3 +1,5 @@
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/DuckDuckGo)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/SimpleGallery)
 $(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/SimpleCalender)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/priv-app/stats)
+$(call add-clean-step, rm -rf $(PRODUCT_OUT)/system/app/WeatherIcons)
